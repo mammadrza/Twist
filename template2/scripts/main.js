a255a491c6ba0266cd1e433fb7c2e561a6e9b533
rza <rza.t@code.edu.az>
2016-10-15 15:42:35 +0400

    twist

---
+++ template2/scripts/main.js
@@ -0,0 +1,108 @@
+
+
+$(document).scroll(function(){
+
+	var my_scroll2 = $(document).scrollTop();
+
+		
+		console.log(my_scroll2);
+		main(my_scroll2)
+})
+
+
+
+$(window).on("load",function(){
+	var my_scroll2 = $(document).scrollTop();
+	main(my_scroll2)
+})
+
+
+
+function main(my_scroll2){
+	if(my_scroll2>100){
+			$(".translate").css({
+				WebkitTransform: 'translateY(0)'
+									})
+							}
+							
+
+
+if(my_scroll2>100){
+			$(".translate2").css({
+				WebkitTransform: 'translateY(0)'
+									})
+							}
+
+
+
+if(my_scroll2>100){
+			$(".translate3").css({
+				WebkitTransform: 'translateY(0)'
+									})
+							}
+
+if(my_scroll2>100){
+			$(".translate4").css({
+				WebkitTransform: 'translateY(0)'
+									})
+							}
+
+if(my_scroll2>100){
+			$(".translate5").css({
+				WebkitTransform: 'translateY(0)'
+									})
+							}
+
+
+
+if(my_scroll2>100){
+			$(".translate6").css({
+				WebkitTransform: 'translateY(0)'
+									})
+							}
+
+
+
+
+							if(my_scroll2>900){
+							$(".div1animate").css({
+							opacity:1,
+							
+		
+												})
+											}
+				if(my_scroll2>1800){
+			$(".final").css({
+				opacity:1,
+
+				// WebkitTransform: 'translateX(0)'
+
+	
+									})
+							if(my_scroll2>1800){
+			$(".final2").css({
+				opacity:1,
+
+				// WebkitTransform: 'translateX(0)'
+
+	
+									})
+							}
+if(my_scroll2>1800){
+			$(".final3").css({
+				opacity:1,
+				
+
+				// WebkitTransform: 'translateX(0)'
+
+	
+									})
+							}
+
+						}
+
+}
+
+
+
+
